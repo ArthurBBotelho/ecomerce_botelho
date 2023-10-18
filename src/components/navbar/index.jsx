9ecf7080b3d97e59aa10ef827cf468a5f3ae42fc
Arthur Botelho <emilioarthur19@gmail.com>
2023-10-18 19:20:53 -0300

    teste de importação

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function navBar() {
     return (
         <div>
-            <img></img>
+            <a><img href="../../image/icon/dice-five.svg"></img></a>
             <h1>Dice Five</h1>
         </div>
     )
